280dcfab4d6f92a465ec0a242d78228a9bc3af7a
KhwajaX <khwaja11@outlook.com>
2021-02-02 23:42:33 +0530

    initial commit

---
+++ untitled.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+			<div class="container">
+                <div class="topnav">
+                    <ul class="quick-menu pull-right clearfix">
+                        <li><a href="dashboard.html">ACCOUNT</a></li>
+                        <li><a href="login.html">LOGIN</a></li>
+                        <li><a href="#travelo-signup" class="soap-popupbox">SIGNUP</a></li>
+                    </ul>
+                </div>
+                
+                <div class="pull-right hidden-mobile">
+                    <address class="contact-details">
+                        <span class="contact-phone"><i class="soap-icon-phone"></i> 91-8908-880844-HELLO</span>
+                        <a class="contact-email" href="#"><i class="soap-icon-letter-1"></i> help@gandharvholidays.com</a>
+                    </address>
+                </div>
+            </div>
+            
+            
+            
+            
+            
+</body>
+</html>
